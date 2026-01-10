fb6c49eaccf8bdc7b920c8f7ecad2baea11c0b02
Geof-0 <geckoeater61@gmail.com>
2026-01-10 13:10:38 -0600

    phone solution part 1
    testing phase

--- phoneScript.js
+++ phoneScript.js
@@ -2,4 +2,17 @@ console.log("js is working (phone js)");
 
 const TemporaryBarrier = document.getElementById("TemporaryBarrier");
 
-TemporaryBarrier.style.height = `${parseInt(TemporaryBarrier.style.height)}px`;
+TemporaryBarrier.style.display = 'None'; // temporary testing (local changes)
+
+
+
+const phoneMenuTitle = document.getElementById("phoneMenuTitle")
+const phoneTitleRects = phoneMenuTitle.getClientRects();
+const phoneTitleRows = phoneTitleRects.length;
+console.log(phoneTitleRows)
+
+const phoneRow1Top =`${(phoneTitleRows * 70) + 70}px`
+
+const phoneRow1 = document.getElementById("phoneRow1")
+phoneRow1.style.top = phoneRow1Top;
+phoneRow1.style.height = `${parseInt(phoneRow1.style.height) - parseInt(phoneRow1Top)}`
