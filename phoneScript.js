f36d871c7b6fd5eb7d919c994d4520b46da1bf08
Geof-0 <geckoeater61@gmail.com>
2026-01-10 13:47:23 -0600

    fixes
    updated so now has an observer for resizing

--- phoneScript.js
+++ phoneScript.js
@@ -4,8 +4,8 @@ const TemporaryBarrier = document.getElementById("TemporaryBarrier");
 
 TemporaryBarrier.style.display = 'None'; // temporary testing (local changes)
 
-
-
+// old code
+/*
 const phoneMenuTitle = document.getElementById("phoneMenuTitle")
 const phoneTitleRects = phoneMenuTitle.getClientRects();
 const phoneTitleRows = phoneTitleRects.length;
@@ -16,3 +16,29 @@ const phoneRow1Top =`${(phoneTitleRows * 70) + 70}px`
 const phoneRow1 = document.getElementById("phoneRow1")
 phoneRow1.style.top = phoneRow1Top;
 phoneRow1.style.height = `${parseInt(phoneRow1.style.height) - parseInt(phoneRow1Top)}`
+*/
+
+const phoneMenuTitle = document.getElementById("phoneMenuTitle")
+
+const phoneSizeChangeObserver = new ResizeObserver(() => {
+    const computedStyle = window.getComputedStyle(phoneMenuTitle)
+
+    const phoneMenuTitleHeight = parseInt(computedStyle.height);
+    const phoneMenuTitleLineHeight = parseInt(computedStyle.fontSize) * 1.2;
+
+    const phoneTitleRows = Math.round(phoneMenuTitleHeight / phoneMenuTitleLineHeight)
+    console.log(computedStyle.height, computedStyle.fontSize, phoneTitleRows)
+
+    const phoneRow1Top =`${(phoneTitleRows * 70) + 100}px`
+
+    const phoneRow1 = document.getElementById("phoneRow1")
+    phoneRow1.style.top = phoneRow1Top;
+    phoneRow1.style.height = `${parseInt(phoneRow1.style.height) - parseInt(phoneRow1Top)}px`
+    if (phoneTitleRows == 1) {
+        phoneRow1.style.height = `${parseInt(phoneRow1.style.height) - 50}px`
+    }
+    
+    console.log('size has changed')
+});
+
+phoneSizeChangeObserver.observe(phoneMenuTitle)
