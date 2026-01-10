ad4e2a359cdd72777b2232ffca00582c63aeffa3
Geof-0 <geckoeater61@gmail.com>
2026-01-09 20:53:33 -0600

    0x20
    did you know that 0x20 was 32 in hexadecimal? i didn't until now

--- phoneScript.js
+++ phoneScript.js
@@ -1,4 +1,5 @@
-console.log("js is working (phone js)")
+console.log("js is working (phone js)");
 
 const TemporaryBarrier = document.getElementById("TemporaryBarrier");
 
+TemporaryBarrier.style.height = `${parseInt(TemporaryBarrier.style.height)}px`;
